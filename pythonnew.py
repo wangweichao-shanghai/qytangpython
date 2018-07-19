33a1900039c143c786ab6d6419d967a29f805373
王维超 <ahjdwwc@163.com>
2018-07-19 11:41:04 +0800

    Initial commit

---
+++ pythonnew.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3.5
+__author__ = 'weichaow'
+import pymongo
+from base64 import b64encode
+from urllib3 import *
+from urllib import parse
+import json
+import re
+disable_warnings()
+http = PoolManager()
+print('welcome to hasco-eds')
+if __name__ == '__main__':
+    pass
